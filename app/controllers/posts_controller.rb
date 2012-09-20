7be48ade0716c4828da3a28ee696f54361c5bfd2
Matthew Powers <matthewkevinpowers@gmail.com>
2012-09-20 10:09:19 -0400

    finished posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,6 +14,27 @@ class PostsController < ApplicationController
     end
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(params[:post])
+      redirect_to @post, :notice =>"Post updated!"
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    if @post.destroy
+      redirect_to posts_path, :notice =>"Post deleted!"
+    end
+  end
+  
 
 end
